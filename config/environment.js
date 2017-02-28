ea883cf8fd4ead08e136607b5168354702f4a53b
Gabriel <gabrielm.jaeger@yahoo.de>
2017-02-28 16:39:30 +0800

    added announcement route. Still working on the individual components

--- config/environment.js
+++ config/environment.js
@@ -10,12 +10,15 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+        }
       },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
-    },
+
+      firebase: {
+        apiKey: "AIzaSyCyPgMJumzytQFfA-tZNmLoRqM_0oyH4nw",
+        authDomain: "emberproject-e7107.firebaseapp.com",
+        databaseURL: "https://emberproject-e7107.firebaseio.com",
+        storageBucket: "emberproject-e7107.appspot.com",
+      },
 
     APP: {
       // Here you can pass flags/options to your application instance
